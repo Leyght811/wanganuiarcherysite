364a3006aba03fd46cad3396570e275afb63f6a0
Leyght811 <wesleyhamish@hotmail.com>
2026-03-13 10:09:12 +1300

    Membership Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
-import ClubPage from './components/pages/ClubPage';
-import BeginnersPage from './components/pages/BeginnersPage';
-import MembershipPage from './components/pages/MembershipPage';
-import GalleryPage from './components/pages/GalleryPage';
-import ContactPage from './components/pages/ContactPage';
-import Navbar from './components/Navbar';
-import background from "./images/background2.jpg"
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import ClubPage from "./components/pages/ClubPage";
+import BeginnersPage from "./components/pages/BeginnersPage";
+import MembershipPage from "./components/pages/MembershipPage";
+import GalleryPage from "./components/pages/GalleryPage";
+import ContactPage from "./components/pages/ContactPage";
+import Navbar from "./components/Navbar";
+import background from "./images/background2.jpg";
+import { useEffect, useState } from "react";
 
 const backgroundStyle = {
     backgroundImage: `url(${background})`,
@@ -18,27 +19,56 @@ const backgroundStyle = {
     backgroundRepeat: "no-repeat", // Optional: prevents the image from repeating
     position: "fixed",
     zIndex: "0",
-    backgroundPosition: "center"
+    backgroundPosition: "center",
 };
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div style={backgroundStyle}></div>
-      {/* Navigation */}
-      <Navbar />
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/club" element={<ClubPage />} />
-        <Route path="/beginners" element={<BeginnersPage />} />
-        <Route path="/membership" element={<MembershipPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    );
 }
+
+const AppContent = () => {
+    const [pageTitle, setPageTitle] = useState("Home");
+
+    const titleMap = [
+        { path: "/", title: "Home" },
+        { path: "/club", title: "Club" },
+        { path: "/beginners", title: "Beginners" },
+        { path: "/membership", title: "Membership" },
+        { path: "/gallery", title: "Gallery" },
+        { path: "/contact", title: "Contact Us" },
+    ];
+
+    let curLoc = useLocation();
+
+    useEffect(() => {
+        const curTitle = titleMap.find((item) => item.path === curLoc.pathname);
+        if (curTitle && curTitle.title) {
+            setPageTitle(curTitle.title);
+            document.title = curTitle.title;
+        }
+    }, [curLoc]);
+
+    return (
+        <div>
+            <div style={backgroundStyle}></div>
+            {/* Navigation */}
+            <Navbar />
+
+            {/* Routes */}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/club" element={<ClubPage />} />
+                <Route path="/beginners" element={<BeginnersPage />} />
+                <Route path="/membership" element={<MembershipPage />} />
+                <Route path="/gallery" element={<GalleryPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
--- src/components/pages/MembershipPage/index.jsx
+++ src/components/pages/MembershipPage/index.jsx
@@ -1,5 +1,42 @@
 function MembershipPage() {
-  return <h1>Membership Page</h1>;
+    return (
+        <div id="membership-page" className="page-container">
+            <div className="page-section">
+                <h3 className="section-head">Membership</h3>
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent quam magna, euismod sit amet ligula quis, tincidunt
+                    blandit quam.
+                    <br />
+                    <br />
+                    Integer ultricies, eros a interdum efficitur, tellus leo
+                    congue turpis, a condimentum lorem turpis at felis. Donec
+                    dapibus sapien massa, id blandit nunc rhoncus sed. Aliquam
+                    erat volutpat. In hac habitasse platea dictumst. Aliquam
+                    vitae tempus arcu. Nulla sagittis egestas porttitor. Sed
+                    mauris nisi, lobortis ac eros ac, ullamcorper rutrum urna.
+                    <br />
+                    <br />
+                    In in blandit nisi, id imperdiet purus. Donec sollicitudin
+                    laoreet elit, non interdum neque vehicula sed. In quam
+                    velit, aliquet eget ante vel, faucibus auctor nibh. Aenean
+                    at ipsum metus. Praesent suscipit urna eget odio dictum
+                    ullamcorper. Maecenas ornare iaculis lectus, eget imperdiet
+                    dolor imperdiet pretium. Proin nec massa vulputate, pharetra
+                    justo non, faucibus justo. Morbi erat tortor, vulputate in
+                    risus eu, aliquet ornare enim. Vestibulum ultrices maximus
+                    dui, ac gravida lorem rhoncus sed. Nulla in neque felis.
+                    Integer ac velit sed diam vulputate bibendum.
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum condimentum malesuada odio, ac pretium enim.
+                    Aenean quis risus augue. Vestibulum ante ipsum primis in
+                    faucibus orci luctus et ultrices posuere cubilia curae; Nunc
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default MembershipPage
+export default MembershipPage;
