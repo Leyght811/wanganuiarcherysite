c751eae50d7dc735b79b65a1dad692b89944c1d6
Leyght811 <wesleyhamish@hotmail.com>
2026-04-08 18:33:26 +1200

    edited extra homelink

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const AppContent = () => {
             {/* Routes */}
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/wanganuiarcherysite" element={<HomePage />} />
+                <Route path="/wanganuiarcherysite/" element={<HomePage />} />
                 <Route path="/club" element={<ClubPage />} />
                 <Route path="/beginners" element={<BeginnersPage />} />
                 <Route path="/membership" element={<MembershipPage />} />
